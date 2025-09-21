43562adce90e846f49495c07af1a42673fc96660
TashaPn <144604987+TashaPn@users.noreply.github.com>
2025-09-21 16:41:37 +0300

    сделала и сохранила 5е

--- src/Lab_01/number_5.py
+++ src/Lab_01/number_5.py
@@ -1,7 +1,8 @@
 print("НОМЕР 5: ИНИЦИАЛЫ И ДЛИНА СТРОКИ")
 full_name = input()
-while full_name.count(" ")!=3:
-    full_name = full_name.replace("  "," ")
-print(full_name)
-
-
+words = full_name.strip().split()
+cleanwords = " ".join(words)
+initials = ''.join(word[0].upper() for word in words) + '.'
+print("ФИО:", full_name)
+print("Инициалы:",initials)
+print("Длина (символов):", len(cleanwords))
