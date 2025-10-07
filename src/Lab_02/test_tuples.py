16308d12890144e482ed92abf1d3d574e4d44d1c
TashaPn <144604987+TashaPn@users.noreply.github.com>
2025-10-08 01:33:44 +0300

    сделала блок C

---
+++ src/Lab_02/test_tuples.py
@@ -0,0 +1,63 @@
+from tuples import format_record
+
+
+def test_format_record():
+    tests = [
+        [
+            ("Иванов Иван Иванович", "BIVT-25", 4.6),
+            "Иванов И.И., гр. BIVT-25, GPA 4.60",
+        ],
+        [
+            ("Петров Пётр", "IKBO-12", 5.0),
+            "Петров П., гр. IKBO-12, GPA 5.00",
+        ],
+        [
+            ("Петров Пётр Петрович", "IKBO-12", 5.0),
+            "Петров П.П., гр. IKBO-12, GPA 5.00",
+        ],
+        [
+            ("  сидорова  анна   сергеевна ", "ABB-01", 3.999),
+            "Сидорова А.С., гр. ABB-01, GPA 4.00",
+        ],
+    ]
+
+    for t in tests:
+        input_record = t[0]
+        result_record = t[1]
+        result = format_record(input_record)
+
+        if result == result_record:
+            print("Test passed, input=", input_record, "result=", result_record)
+        else:
+            print("Test failed, input=", input_record, "result=", result,"result_record=", result_record)
+
+
+def test_format_record_value_error():
+    try:
+        format_record(("", 'IKBO-12', 5.0))
+    except ValueError:
+        print("test passed: value error")
+    else:
+        print("test failed: value error")
+
+    try:
+        format_record(("Иванов Иван Иванович", '', 5.0))
+    except ValueError:
+        print("test passed: value error")
+    else:
+        print("test failed: value error")
+
+
+def test_format_record_type_error():
+    try:
+        format_record(("Иванов Иван Иванович", 'IKBO-12', "5.0"))
+    except TypeError:
+        print("test passed: type error")
+    else:
+        print("test failed: type error")
+
+
+print("TEST FORMAT-RECORD")
+test_format_record()
+test_format_record_value_error()
+test_format_record_type_error()
---
+++ src/Lab_02/tuples.py
@@ -0,0 +1,32 @@
+def format_record(rec: tuple[str, str, float]) -> str:
+    """
+    Работаем с «записями» как с кортежами
+    
+    Вызывает ValueError при пустом значении ФИО или группы
+    Вызывает TypeError при неверном типе (не float) GPA
+    """
+    fio = rec[0]
+    group = rec[1]
+    gpa = rec[2]
+    
+    if fio=="" or group=="":
+        raise ValueError
+    if type(gpa)!=float:
+        raise TypeError
+
+    fio = fio.split()
+
+    for n in range(len(fio)):
+        s = fio[n]
+        s = s.capitalize()
+
+        if n > 0:
+            s = f"{s[0]}."
+
+        fio[n] = s
+    
+    name = fio[0]
+    surname = "".join(fio[1:])
+
+    return f"{name} {surname}, гр. {group}, GPA {gpa:.2f}"
+
