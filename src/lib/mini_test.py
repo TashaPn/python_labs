6486039b98b120c2f48a281dcaf7a57f2cd5a585
TashaPn <144604987+TashaPn@users.noreply.github.com>
2025-10-30 19:39:13 +0300

    закончила делать 3ю лабу

--- src/lib/mini_test.py
+++ src/lib/mini_test.py
@@ -1,4 +1,4 @@
-from src.lib.io_txt_csv import read_text, write_csv
+from lib.io_txt_csv import read_text, write_csv
 
 txt = read_text("src/data/input.txt")  # должен вернуть строку
 f_csv = write_csv([("word","count"),("test",3)], "src/data/check.csv")  # создаст CSV
