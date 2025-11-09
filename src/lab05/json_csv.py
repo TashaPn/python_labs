299b92ec72c6b72fd523479f8345d054f14f394e
TashaPn <144604987+TashaPn@users.noreply.github.com>
2025-11-09 22:37:49 +0300

    выполнила требования для первой функции

--- src/lab05/json_csv.py
+++ src/lab05/json_csv.py
@@ -3,12 +3,49 @@ from lab04.io_txt_csv import write_csv
 
 from pathlib import Path
 
+def convert(j: list) -> list:
+    """
+    Переводит данные, которые считали из json в формат для csv
+    """
+    result = []
+    result1 = []   # заголовок таблицы
+
+    slovar = j[0]
+    if type(slovar)!= dict:
+        raise ValueError
+    
+    for key in slovar:
+        result1.append(key)
+        
+    result.append(result1)
+    
+    # result == [
+    #     ["A", "B", "C", "D"],  # заголовок
+    # ]
+
+    
+    for s in j:
+        if type(s)!= dict:
+            raise ValueError
+        result2 = []
+        for key in result1:
+            value = s.get(key)
+            if value == None:
+                value = ""
+            result2.append(value)
+        result.append(result2)
+ 
+    return result
+
 
 def json_to_csv(json_path: str, csv_path: str) -> None:
     """
     Преобразует JSON-файл в CSV.
     Поддерживает список словарей [{...}, {...}], заполняет отсутствующие поля пустыми строками.
     Кодировка UTF-8. Порядок колонок — как в первом объекте или алфавитный (указать в README).
+
+    json_path: путь до файла на диске, из которого мы читаем данные в формате json
+    csv_path: путь до файла на диске, в который мы пишем данные в фомате csv
     """
     
     p = Path(json_path)
@@ -20,20 +57,12 @@ def json_to_csv(json_path: str, csv_path: str) -> None:
     try:
         json_f = json.load(fp)
     except json.decoder.JSONDecodeError:
-        raise ValueError("пустой/неверный JSON")
+        raise ValueError("Пустой JSON или неподдерживаемая структура")
     
-    csv_file = []
     
-    for i in json_f:
-        name = i["name"]
-        age = i["age"]
-        city = i["city"]
-        
-        csv_file.append([name, age, city])
-    
-    write_csv(csv_file, csv_path, ["Name","Age","City"]) 
+    massive = convert(json_f)
+    write_csv(massive, csv_path)
 
 
-
-json_to_csv("data/lab05/samples/people_empty.json","data/lab05/persons2.csv")
+json_to_csv("data/lab05/samples/people_invalid.json","data/lab05/persons2.csv")
 
