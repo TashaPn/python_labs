f6b93136170c0dac3b10ec330b809ec4dd5dea92
TashaPn <144604987+TashaPn@users.noreply.github.com>
2025-11-21 17:14:16 +0300

    начинаю делать 6ю лабу

---
+++ src/lab06/cli_convert.py
@@ -0,0 +1,12 @@
+from src.lab05.json_csv import *
+from src.lab05.csv_to_xlsx import *
+
+
+def main2():
+    # подкоманда json2csv 
+
+    # подкоманда csv2json
+
+    # подкоманда csv2xlsx
+
+    pass
---
+++ src/lab06/test.py
@@ -0,0 +1,19 @@
+import pytest
+
+from src.lab06.cli_text import *
+from src.lab06.cli_convert import *
+
+def test_cat():
+    main1()
+
+def test_stats():
+    main1()
+
+def test_json2csv():
+    main2()
+
+def test_csv2json():
+    main2()
+
+def test_csv2xlsx():
+    main2()
