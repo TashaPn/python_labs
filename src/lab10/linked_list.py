92328cf38bf48a3a992c9ec85d357758486260e8
TashaPn <144604987+TashaPn@users.noreply.github.com>
2025-12-16 21:47:32 +0300

    сделала задание B 10й лабы

--- src/lab10/linked_list.py
+++ src/lab10/linked_list.py
@@ -7,29 +7,36 @@ class Node:
 class SinglyLinkedList:
     def __init__(self):
         self.head = None
-        # ошибка: размер не обновляется
         self._size = 0
 
     def append(self, value):
         """Добавить элемент в конец списка"""
         new_node = Node(value)
-        if self.head is None:
+        
+        # Если список пустой, создаем новую ноду в self.head
+        if self.head == None:
             self.head = new_node
-            # TODO: добавить увеличение размера
-            return
+            self._size += 1
+        else:
 
-        # неэффективность: полный обход списка O(n)
-        current = self.head
-        while current.next is not None:
-            current = current.next
-        current.next = new_node
-        # TODO: self._size += 1
+        # Если не пустой, берем первую ноду из self.head
+            current_node = self.head
+
+            # Начинаем идти по всем .next, пока там не будет None
+            # Это значит, что мы нашли последнюю ноду в списке, за ней ничего нет
+            while current_node.next != None:
+                current_node = current_node.next
+            
+            # В последнюю ноду в .next сохраняем новую ноду
+            current_node.next = new_node
+            self._size += 1
+
 
     def prepend(self, value):
         """Добавить элемент в начало списка"""
         new_node = Node(value, next=self.head)
         self.head = new_node
-        # TODO: обновить размер
+        self._size += 1
 
     def insert(self, idx, value):
         """Вставка по индексу — неполная реализация, есть ошибки"""
@@ -40,14 +47,19 @@ class SinglyLinkedList:
             self.prepend(value)
             return
 
-        current = self.head
-        # ошибка: нет проверки выхода за границы
-        for _ in range(idx - 1):
-            current = current.next
+        if idx > self._size:
+            raise IndexError("too big index is not supported")
 
-        new_node = Node(value, next=current.next)
-        current.next = new_node
-        # TODO: увеличить размер
+        new_node = Node(value)
+
+        current_node = self.head
+        for step in range(idx-1):
+            current_node = current_node.next
+        
+        new_node.next = current_node.next
+        current_node.next = new_node
+
+        self._size += 1
 
     def __iter__(self):
         current = self.head
@@ -56,9 +68,29 @@ class SinglyLinkedList:
             current = current.next
 
     def __len__(self):
-        # ошибка: всегда возвращает 0
         return self._size
 
     def __repr__(self):
         values = list(self)
-        return f"SinglyLinkedList({values})"
+        return f"SinglyLinkedList({values})" 
+    
+    def remove(self, value) -> None:
+        """
+        удалить первое вхождение значения value
+        """
+        current_node = self.head
+        previuos_node = None
+        found = False
+
+        while current_node is not None:
+            if current_node.value == value :
+                found = True
+                break
+
+            previuos_node = current_node
+            current_node = current_node.next
+        
+        if found:
+            next_node = current_node.next
+            previuos_node.next = next_node
+            self._size -= 1
---
+++ tests/test_linked_list.py
@@ -0,0 +1,67 @@
+import pytest
+
+from src.lab10.linked_list import SinglyLinkedList
+
+
+@pytest.fixture
+def si():
+    si = SinglyLinkedList()
+    si.append(333)
+    si.append("Yes")
+    return si
+
+
+def test_append_node():
+    si = SinglyLinkedList()
+    si.append(333) 
+    assert si._size == 1
+    node = si.head
+    assert node.value == 333
+
+def test_four_append_node(si):
+    si.append("No")
+    si.append("Maybe")
+    assert si._size == 4
+    assert si.head.value == 333
+    assert si.head.next.value == "Yes"
+    assert si.head.next.next.value == "No"
+    assert si.head.next.next.next.value == "Maybe"
+
+def test_pretend_node(si):
+    si.prepend("No")
+    assert si.head.value == "No"
+    assert si.head.next.value == 333
+    assert si.head.next.next.value == "Yes"
+    assert len(si) == 3
+
+def test_insert_node(si):
+    si.append("No")
+    si.insert(1, "Maybe")
+    assert si.head.value == 333
+    assert si.head.next.value == "Maybe"
+    assert si.head.next.next.value == "Yes"
+    assert si.head.next.next.next.value == "No"
+    assert len(si) == 4
+
+def test_iter(si):
+    si.append("No")
+
+    nodes = list(si)
+    assert nodes == [333, "Yes", "No"]
+
+def test_remove_node(si):
+    si.append("No")
+
+    si.remove("Yes")
+    assert si.head.value == 333
+    assert si.head.next.value == "No"
+    assert len(si) == 2
+
+def test_remove_invalid_node(si):
+    si.append("No")
+
+    si.remove("Maybe")
+    assert si.head.value == 333
+    assert si.head.next.value == "Yes"
+    assert si.head.next.next.value == "No"
+    assert len(si) == 3
